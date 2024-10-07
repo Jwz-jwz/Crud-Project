b8b51a11afeef483feaf8e70c0508cc95986d020
Жавзандулам Санжаасүрэн <24LP4023@G401-CC0100.local>
2024-10-07 09:52:43 +0800

    not-finished

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,17 +1,12 @@
-export const ProducCard = ({
-  product,
-  selectedProduct,
-  setSelectedProduct,
-  setProducts,
-}) => {
-  const { productName, category, price } = products;
+export const ProductCards = ({}) => {
+  // const { productName, category, price } = products ;
   return (
     <div className="card bg-neutral text-neutral-content w-96">
       <div className="card-body items-center text-center">
         <h2 className="card-title">New product!</h2>
-        <p>{productName}</p>
-        <p>{category}</p>
-        <p>{price}</p>
+        <p></p>
+        <p></p>
+        <p></p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Back</button>
           <button className="btn btn-ghost">Edit</button>
