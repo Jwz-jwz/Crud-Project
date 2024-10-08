4cfd7ae698d0764420e8d449c5ae3efcf7299538
Jwz-jwz <jnglobal444@gmail.com>
2024-10-08 22:05:15 +0800

    dll

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,29 +1,81 @@
-import { DeleteIcon } from "@/app/svg/DeletIcon";
-import { EditIcon } from "@/app/svg/EditIcon";
+import { EditModal } from "./EditProduct";
 
 export const ProductCard = ({
-  handleDelete,
   product,
-  setProducts,
-  setSelectedProduct,
   selectedProduct,
+  setSelectedProduct,
+  setProducts,
 }) => {
   const { productName, category, price } = product;
+
+  const handleDelete = async (id) => {
+    try {
+      const options = {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: id }),
+      };
+      const response = await fetch(`${BACKEND_POINT}/product`, options);
+      const data = await response.json();
+      console.log(id);
+
+      setProducts((prevProducts) =>
+        prevProducts.filter((product) => data?.product?.id !== product?.id)
+      );
+    } catch {
+      console.log("error");
+    }
+    // document.getElementById("my_modal_2").close();
+  };
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      const options = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(selectedProduct),
+      };
+      const response = await fetch(`${BACKEND_ENDPOINT}/product`, options);
+      const data = await response.json(response);
+      setProducts(data.products);
+    } catch {
+      console.log("error");
+    }
+    document.getElementById("my_modal_2").close();
+  };
+
+  const handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setSelectedProduct((prevProduct) => {
+      return {
+        ...prevProduct,
+        [name]: value,
+      };
+    });
+  };
+
   return (
-    <div className="w-[364px] rounded-[20px] flex flex-col gap-4 p-6 border border-[#F4F4F4] bg-green-100">
+    <div className="w-[364px] rounded-[20px] flex flex-col gap-4 p-6 border border-[#F4F4F4] bg-green-200">
       <div className="flex justify-between">
         <p>Барааны нэр: {productName}</p>
-        <div className="flex gap-2 items-center">
-          <button onClick={() => handleDelete(product?.id)}>
-            <DeleteIcon />
-          </button>
-          <button>
-            <EditIcon />
-          </button>
-        </div>
       </div>
       <p>Барааны ангилал: {category}</p>
       <p>Барааны үнэ: {price}</p>
+      <div className="flex gap-2 items-center justify-end">
+        <button onClick={() => handleDelete(product?.id)} className="btn">
+          Delete
+        </button>
+        <EditModal
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+        />
+      </div>
     </div>
   );
 };
